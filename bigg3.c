29366e306b75312686c03b341833df0c4d75cbd5
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-01-28 16:11:53 +0530

    Create bigg3.c

---
+++ bigg3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    char c;
+    printf("enter the char");
+    scanf("%c",&c);
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("consonant");
+    }
+    getch();
+    }
+    
