1bb18df4e04051ae45ed83f52e1effc2504345ae
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-02-20 11:06:15 -0500

    Create bigg23.c

---
+++ bigg23.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i,count=0;
+scanf("%d",&n);
+for(i=2;i<a;i++)
+{
+if(a%i==0)
+count++;
+}
+if(count==0)
+printf("prime number");
+else
+printf("not a prime number");
+getch();
+}
