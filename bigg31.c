ff25f630470f3315c93e656085b0877c137fa442
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-02-27 12:17:22 +0530

    Create bigg31.c

---
+++ bigg31.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,t,r,sum,k;
+printf("enter the number");
+scanf("%d",&s);
+printf("enter the number");
+scanf("%d",&t);
+printf("enter the number");
+scanf("%d",&r);
+k=(r*t)+s-1;
+sum=((s+k)*r)/2;
+printf("The A.P is");
+}
