48dcc065a657b0dfb989ee4f48f6057c3c64a70d
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-02-27 12:11:18 +0530

    Create bigg30.c

---
+++ bigg30.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int hr1,min1,hr2,min2,hr,min;
+ printf("/n enter the first time");
+ scanf("%d%d",&hr1,&min1);
+ printf("/n enter the second time");
+ scanf("%d%d",&hr2,&min2);
+ hr=hr1-hr2;
+ min=min1-min2;
+ printf("/n the time is %d:%d",hr,min);
+ getch();
+} 
