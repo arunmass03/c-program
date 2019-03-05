012613b2578d1ea9128a8d0d73873d744089b4e0
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-05 23:12:45 +0530

    Create bigg79.c

---
+++ bigg79.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+    int n,m,diff=0;
+    scanf("%d %d",&n,&m);
+    if(n>m)
+    {
+        diff=n-m;
+    }
+    else
+    {
+        diff=m-n;
+    }
+    if(diff%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+	return 0;
+}
