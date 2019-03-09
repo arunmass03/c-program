d99141bc740aa14cdb30e72e3354e8314831749b
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-09 21:47:58 +0530

    Create Bigg93.c

---
+++ Bigg93.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{	
+   int n1,n2,i;
+   char c;
+   for(i=0;i<4;i++)
+   {
+       scanf("%d %c %d",&n1,&c,&n2);
+       if(c=='/')
+       {
+           printf("%d\n",(n1/n2));
+       }
+       else
+       {
+           printf("%d\n",(n1%n2));
+       }
+   }
+	return 
