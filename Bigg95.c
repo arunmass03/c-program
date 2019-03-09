dd2414b9bcbb514fbfd019c98b5bc1115f9a8a30
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-09 22:52:36 +0530

    Create Bigg95.c

---
+++ Bigg95.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() 
+{	
+   char p[10],q[10];
+   int i,n;
+   scanf("%s",p);
+   n=strlen(a);
+   for(i=0;i<n;i=i+2)
+   {
+       q[i]=p[i];
+       printf("%c",q[i]);
+   }
+   printf(" ");
+   for(i=1;i<n;i=i+2)
+   {
+       q[i]=p[i];
+       printf("%c",b[i]);
+   }
+ return 0;
+}
