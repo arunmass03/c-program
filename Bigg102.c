627ac04f22663f79963337a0753a2ad518ba2730
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-09 23:11:28 +0530

    Create Bigg102.c

---
+++ Bigg102.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,a[n],i,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a);
+	    sum=sum+a;
+	}
+	printf("%d",sum);
+}
