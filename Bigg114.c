e79c555a8b497a34214d09743cbcfd4dec476822
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-09 23:42:13 +0530

    Create Bigg114.c

---
+++ Bigg114.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,l,res=1,i;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<l;i++)
+    {
+        res=res*n;
+    }
+    printf("%d",res);
+	return 0;
+}
