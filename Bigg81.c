f735eec9a7ba1cf696cc8e1d1ac4146f5002ae53
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-09 21:11:05 +0530

    Create Bigg81.c

---
+++ Bigg81.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int n,i,count=0;
+    scanf("%s",a);
+    n=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==a[n-i-1])
+        {
+            count++;
+        }
+    }
+    if(n==count)
+    {
+        printf("yes..it is palindrome");
+    }
+    else
+    {
+        printf("not");
+    }
+	return 0;
+}
