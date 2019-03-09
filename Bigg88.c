23779d703b644485a32bce0544faad80a49dbe94
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-09 21:34:03 +0530

    Create Bigg88.c

---
+++ Bigg88.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() 
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes...%d is a multiple of 13",n);
+    }
+    else
+    {
+    printf("no");
+    }
+	return 0;
+}
