bc934b9437a5e305394490b03d9554407ea8101d
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-17 18:15:19 +0530

    Create Play3.c

---
+++ Play3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+    int a[10],i,n,rev=0,rem;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        rev=(rev*10)+rem;
+        n=n/10;
+    }
+    printf("%d",rev);
+}
