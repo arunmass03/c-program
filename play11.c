52b9d7f6838caa16516481e906802bed279dff16
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-19 16:04:55 +0530

    Create play11.c

---
+++ play11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[10];
+	scanf("%s",a);
+	if(a[0]=='S'||a[0]=='s')
+	{
+	    printf("yes...holiday");
+	}
+	else
+	{
+	 printf("no..not a holiday");   
+	}
+}
