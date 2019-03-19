7413bb726046f7998780288b3c712ca7ad001d40
arunmass03 <46813455+arunmass03@users.noreply.github.com>
2019-03-19 15:58:11 +0530

    Rename play10.c to play 10.c

---
+++ play 10.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main() 
+{	
+char a[10],b[10];	
+int i,j,n,n1,count=0;	
+scanf("%s %s",a,b);	
+n=strlen(a);	
+n1=strlen(b);	
+if(n==n1)	
+{   
+for(i=0,j=0;i<n,j<n1;i++,j++)   
+{    	    
+if(a[i]==b[j])    	   
+{    	       
+continue;    	  
+}    	   
+else    	  
+{
+count++;    	  
+}   
+}    	
+if(count==1)  
+{    	    
+printf("yes..differ by 1 character");    
+}	
+}
+else	
+{
+printf("false");
+}	
+}
